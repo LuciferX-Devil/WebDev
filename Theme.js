95e6b3bf4bd1a5598a7eaba4216122babdbcb1d7
LUCIFER <102866831+LuciferX-Devil@users.noreply.github.com>
2025-04-05 16:08:48 +0530

    Create Theme.js

---
+++ Theme.js
@@ -0,0 +1,60 @@
+function themeMars(){
+    document.getElementsByClassName("main")[0].style.backgroundColor = "#472111"
+    document.getElementsByClassName("globs")[0].style.backgroundColor = "#4a2312"
+    document.getElementById("side").style.backgroundImage = "linear-gradient(#180a02,#321a0a)"
+    document.getElementById("main").style.backgroundImage = "linear-gradient(#220c00,#392013)"
+    document.getElementsByClassName("profoot")[0].style.backgroundColor = "rgb(27, 10, 4)"
+    document.documentElement.style.setProperty("--thumbColor1","rgb(25, 5, 5)")
+    document.getElementsByClassName("buttons")[0].style.backgroundColor = "#220c00"
+    document.getElementsByClassName("Message_srch")[0].style.backgroundColor = "#421c0e"
+    document.documentElement.style.setProperty("--thumbColor2","#ef9898")
+    document.documentElement.style.setProperty("--trackColor2","#823520")
+    document.getElementsByClassName("LeftUI")[0].style.backgroundImage = "linear-gradient(to right,#361306,#271007)"
+    document.getElementsByClassName("RightUI")[0].style.backgroundColor = "#271007"
+}
+
+function themeMidBlu(){
+    document.getElementsByClassName("main")[0].style.backgroundColor = "#212082"
+    document.getElementsByClassName("globs")[0].style.backgroundColor = "#1a1884"
+    document.getElementById("side").style.backgroundImage = "linear-gradient(#0B0A2A,#0E1342)"
+    document.getElementById("main").style.backgroundImage = "linear-gradient(#030132,#0d0c28)"
+    document.getElementsByClassName("profoot")[0].style.backgroundColor = "rgb(4, 4, 27)"
+    document.documentElement.style.setProperty("--thumbColor1","rgb(5, 7, 25)")
+    document.getElementsByClassName("buttons")[0].style.backgroundColor = "#030132"
+    document.getElementsByClassName("Message_srch")[0].style.backgroundColor = "#0E1342"
+    document.documentElement.style.setProperty("--thumbColor2","#989FEF")
+    document.documentElement.style.setProperty("--trackColor2","#212082")
+    document.getElementsByClassName("LeftUI")[0].style.backgroundImage = "linear-gradient(to right,#0E1342,#0B0A2A)"
+    document.getElementsByClassName("RightUI")[0].style.backgroundColor = "#0B0A2A"
+}
+
+function themeLitGreen(){
+    document.getElementsByClassName("main")[0].style.backgroundColor = "rgb(173, 245, 210)"
+    document.getElementsByClassName("globs")[0].style.backgroundColor = "rgb(147, 211, 154)"
+    document.getElementById("side").style.backgroundImage = "linear-gradient(rgb(42, 247, 185),rgb(150, 223, 201))"
+    document.getElementById("main").style.backgroundImage = "linear-gradient(rgb(105, 213, 120),rgb(132, 241, 188))"
+    document.getElementsByClassName("profoot")[0].style.backgroundColor = "rgb(162, 255, 212)"
+    document.documentElement.style.setProperty("--thumbColor1","rgb(30, 148, 60)")
+    document.getElementsByClassName("buttons")[0].style.backgroundColor = "rgb(105, 213, 120)"
+    document.getElementsByClassName("Message_srch")[0].style.backgroundColor = "rgb(53, 247, 195)"
+    document.documentElement.style.setProperty("--thumbColor2","rgb(112, 231, 201)")
+    document.documentElement.style.setProperty("--trackColor2","rgb(15, 191, 83)")
+    document.getElementsByClassName("LeftUI")[0].style.backgroundImage = "linear-gradient(to right,rgb(0, 240, 156),rgb(0, 200, 117))"
+    document.getElementsByClassName("RightUI")[0].style.backgroundColor = "rgb(0,200,117)"
+}
+
+
+function themeMariGold(){
+    document.getElementsByClassName("main")[0].style.backgroundColor = "rgb(238, 192, 11)"
+    document.getElementsByClassName("globs")[0].style.backgroundColor = "rgb(147, 211, 154)"
+    document.getElementById("side").style.backgroundImage = "linear-gradient(rgb(200, 168, 24),rgb(221, 179, 94))"
+    document.getElementById("main").style.backgroundImage = "linear-gradient(rgb(182, 132, 24),rgb(240, 208, 110))"
+    document.getElementsByClassName("profoot")[0].style.backgroundColor = "rgb(118, 93, 21)"
+    document.documentElement.style.setProperty("--thumbColor1","rgb(30, 148, 60)")
+    document.getElementsByClassName("buttons")[0].style.backgroundColor = "rgb(182, 132, 24)"
+    document.getElementsByClassName("Message_srch")[0].style.backgroundColor = "rgb(53, 247, 195)"
+    document.documentElement.style.setProperty("--thumbColor2","rgb(112, 231, 201)")
+    document.documentElement.style.setProperty("--trackColor2","rgb(15, 191, 83)")
+    document.getElementsByClassName("LeftUI")[0].style.backgroundImage = "linear-gradient(to right,rgb(0, 240, 156),rgb(0, 200, 117))"
+    document.getElementsByClassName("RightUI")[0].style.backgroundColor = "rgb(0,200,117)"
+}
