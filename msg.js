aa50a81b447f4b69a485694bbf05468cd886ae48
LUCIFER <102866831+LuciferX-Devil@users.noreply.github.com>
2025-04-10 11:16:42 +0530

    Update msg.js

--- msg.js
+++ msg.js
@@ -1,17 +1,18 @@
 function createMsg(time,msg){
     const div=document.createElement("div")
+    div.className="every_msg"
     div.innerHTML = `
         <div class="message">
-            <img class="msg-pfp" src="https://i.postimg.cc/HLLTdqSM/pfp.png" alt="pfp">
             <div class="msg-body">
                 <div class="msg-header">
-                    <span class="msg-user">Member_1</span>
+                    <span class="msg-user">LUCIFER</span>
                     <span class="msg-time">${time}</span>
                 </div>
                 <div class="msg-content">
                     ${msg}
                 </div>
             </div>
+            <img class="msg-pfp" src="https://i.postimg.cc/HLLTdqSM/pfp.png" alt="pfp">
         </div>
     `
     return div
@@ -23,13 +24,36 @@ function addMsg() {
     const msgContent = inp.value
     const timeStamp = new Date().toLocaleTimeString()
     const messageDiv = createMsg(timeStamp, msgContent)
-    document.getElementById("messages").appendChild(messageDiv)
+    document.getElementById("chat").appendChild(messageDiv)
     inp.value = ""
 }
 
+function receiveMsg(message){
+    const time = new Date().toLocaleTimeString()
+    const div = document.createElement("div")
+    div.id="every_msg"
+    div.innerHTML = `
+        <div class="rec_msg">
+            <img class="msg-pfp" src="https://i.postimg.cc/HLLTdqSM/pfp.png">
+            <div class="rec-msg-body">
+                <div class="msg-header">
+                    <span class="msg-user">Member_1</span>
+                    <span class="msg-time">${time}</span>
+                </div>
+                <div class="msg-content">
+                    ${message}
+                </div>
+            </div>
+        </div>
+    `
+    document.getElementById("chat").appendChild(div)
+}
+
+
+
 document.getElementById("inp").addEventListener("keydown", (event) => {
-    console.log(event.key);
     if (event.key === "Enter") {
         addMsg();
     }
 });
+
